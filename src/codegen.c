9b777b84196cab17367fa47c13afbed601d29591
xsnieh00 <xsnieh00@stud.fit.vutbr.cz>
2022-12-05 22:18:10 +0100

    Added en_declare_var function

--- src/codegen.c
+++ src/codegen.c
@@ -2,20 +2,36 @@
 
 
 
-
-
-
-
-int gen_code(RULE_T * rule) {
-    return 0;
+int gen_declare_var(TOKEN_T* token, int scope) {
+    
 }
+
+
 
 // token = expression
 int gen_expression(TOKEN_T* token, BSTnode* node)
 {
+    // printf(">%d\n", node->type);
+    printf(">>>>>>CODEGEN>>>>>> DEFVAR LF@%s\n", token->name);
+    // BST_print(node, 2);
+    if (node->token->type == LITERAL) {
+
+        switch (node->token->keyword) {
+        case KEY_INT:
+            printf(">>>>>>CODEGEN>>>>>> MOVE LF@%s int@%d\n", token->name, node->token->value.int_val);
+            break;
+        
+        default:
+            break;
+        }
+
+    } else {
+
+    }
+
     return 0;
 }
 
 void gen_if(BSTnode* conditionTree){
-
+    printf("\n\n\nCall of gen_if\n\n\n");
 }
--- src/codegen.h
+++ src/codegen.h
@@ -14,5 +14,6 @@
 // int gen_code(RULE_T * rule);
 int gen_expression(TOKEN_T* token, BSTnode* node);
 void gen_if(BSTnode* conditionTree);
+int gen_declare_var(TOKEN_T* token, int scope);
 
 #endif
